ef1a00d35b7896e6f2cf9daa6a1c59ea636ce800
bojanadrcic <bojanadrcic02@gmail.com>
2025-12-04 18:14:59 +0100

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ArtiklComponent } from './main/artikl/artikl.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ArtiklComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
